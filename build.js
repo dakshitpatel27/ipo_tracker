589bad63ab24bb227f58091b84657a3a8dbaa513
dakshitpatel27 <gajiparadakshit@gmail.com>
2026-07-19 22:21:55 +0530

    Fix Vercel static output directory

--- build.js
+++ build.js
@@ -5,9 +5,9 @@ const { execSync } = require('child_process');
 console.log('Building frontend...');
 execSync('npm install && npm run build', { cwd: path.join(__dirname, 'frontend'), stdio: 'inherit' });
 
-console.log('Copying frontend/dist to dist...');
+console.log('Copying frontend/dist to public...');
 const src = path.join(__dirname, 'frontend', 'dist');
-const dest = path.join(__dirname, 'dist');
+const dest = path.join(__dirname, 'public');
 
 if (fs.existsSync(dest)) {
     fs.rmSync(dest, { recursive: true, force: true });
